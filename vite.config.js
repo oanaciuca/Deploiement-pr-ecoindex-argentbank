e8cf5996036cf12b69188b6f7e34d40364b6b43e
oanaciuca <oana.daniela.ciuca@gmail>
2025-02-13 17:49:17 +0100

    Mise à jour

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    visualizer({
+      open: true,                // Ouvre automatiquement le rapport dans ton navigateur après le build
+      filename: 'bundle-analysis.html', // Nom du fichier généré pour le rapport
+      gzipSize: true,            // Affiche également la taille gzip
+      brotliSize: true           // Affiche la taille Brotli
+    })  
+  ],
 })
